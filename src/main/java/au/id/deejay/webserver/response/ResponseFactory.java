978875ea1ef02e76a6d23625c27ee4ebaf86553e
David Jessup <david.jessup@sydney.edu.au>
2016-10-10 16:31:56 +1100

    ResponseFactory now re-wraps any exceptions emitted from a RequestHandler in a ResponseException for upstream handling

--- src/main/java/au/id/deejay/webserver/response/ResponseFactory.java
+++ src/main/java/au/id/deejay/webserver/response/ResponseFactory.java
@@ -3,6 +3,7 @@ package au.id.deejay.webserver.response;
 import au.id.deejay.webserver.api.Request;
 import au.id.deejay.webserver.api.RequestHandler;
 import au.id.deejay.webserver.api.Response;
+import au.id.deejay.webserver.exception.ResponseException;
 
 import java.util.List;
 
@@ -18,18 +19,38 @@ public class ResponseFactory {
 
 	private List<RequestHandler> requestHandlers;
 
+	/**
+	 * Constructs a new response factory and registers the provided {@link RequestHandler}s to handle generation of
+	 * responses. The order of the handler list is significant as handlers will be asked in order if they can provide a
+	 * response to a request.
+	 *
+	 * @param requestHandlers the list of request handlers to register for response generation.
+	 */
 	public ResponseFactory(List<RequestHandler> requestHandlers) {
 		this.requestHandlers = requestHandlers;
 	}
 
+	/**
+	 * Generates a response to a request by asking each of the registered {@link RequestHandler}s in turn if they can
+	 * handle the request. If no handler opts to provide a response a 501-Not-Implemented response is returned.
+	 *
+	 * @param request the request to be responded to
+	 * @return Returns a response to the request.
+	 * @throws ResponseException if an exception is thrown by any of the {@link RequestHandler}s.
+	 */
 	public Response response(Request request) {
 
 		Response response = null;
 
-		for (RequestHandler handler : requestHandlers) {
-			if (handler.canHandle(request)) {
-				response = handler.handle(request);
+		try {
+			for (RequestHandler handler : requestHandlers) {
+				if (handler.canHandle(request)) {
+					response = handler.handle(request);
+				}
 			}
+		} catch (Exception e) {
+			// Wrap the exception for upstream handling
+			throw new ResponseException(e);
 		}
 
 		if (response == null) {

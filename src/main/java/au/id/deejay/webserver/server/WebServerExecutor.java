97b8faf7efbfbf4dcbd3733965463aa6563584f5
David Jessup <david.jessup@sydney.edu.au>
2016-10-10 22:16:11 +1100

    Refactored WebServerExecutor to better handle shutdown events, added test coverage

--- src/main/java/au/id/deejay/webserver/server/WebServerExecutor.java
+++ src/main/java/au/id/deejay/webserver/server/WebServerExecutor.java
@@ -1,6 +1,5 @@
 package au.id.deejay.webserver.server;
 
-import au.id.deejay.webserver.exception.ServerException;
 import au.id.deejay.webserver.response.ResponseFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +8,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -21,12 +21,10 @@ import java.util.concurrent.TimeUnit;
 public class WebServerExecutor implements Runnable {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WebServerExecutor.class);
-
+	private final ResponseFactory responseFactory;
 	private int port;
 	private int timeout;
 	private int maxThreads;
-	private final ResponseFactory responseFactory;
-
 	private ExecutorService threadPool;
 	private boolean running;
 
@@ -45,45 +43,39 @@ public class WebServerExecutor implements Runnable {
 
 		threadPool = Executors.newFixedThreadPool(maxThreads);
 
-		ServerSocket serverSocket = serverSocket();
-		while (running()) {
-			// Wait for a client connection
-			Socket client = acceptConnection(serverSocket);
+		try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-			if (client != null) {
-				// Assign the connection to a worker
-				Runnable worker = assignWorker(client);
+			// Only block for 1 second so the running loop can escape if the server is stopped.
+			serverSocket.setSoTimeout(1000);
 
-				// Queue the worker for execution
-				threadPool.execute(worker);
+			LOG.info("Server listening on port {}", serverSocket.getLocalPort());
+
+			while (running()) {
+				handleConnection(serverSocket);
 			}
+		} catch (IOException e) {
+			LOG.warn("Error listening for client connection.", e);
+			stop();
 		}
+	}
 
-		LOG.info("Shutting down web server executor.");
-
+	private void handleConnection(ServerSocket serverSocket) throws IOException {
 		try {
-			threadPool.shutdown();
-			threadPool.awaitTermination(timeout, TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			LOG.error("Worker thread pool was interrupted while shutting down. Some client connections may have been terminated prematurely.", e);
-			Thread.currentThread().interrupt();
-		} finally {
-			try {
-				serverSocket.close();
-			} catch (IOException e) {
-				LOG.error("Failed to release server port", e);
-			}
-		}
+			// Wait for a client connection
+			Socket client = serverSocket.accept();
 
-		LOG.info("Web server executor has shutdown.");
+			// Assign the connection to a worker
+			Runnable worker = assignWorker(client);
+
+			// Queue the worker for execution
+			threadPool.execute(worker);
+		} catch (SocketTimeoutException e) {
+			// Swallow the exception and allow the loop to continue
+		}
 	}
 
 	public synchronized boolean running() {
 		return running;
-	}
-
-	public synchronized void stop() {
-		running = false;
 	}
 
 	private Runnable assignWorker(Socket client) {
@@ -95,21 +87,20 @@ public class WebServerExecutor implements Runnable {
 		return new WebWorker(client, responseFactory);
 	}
 
-	private ServerSocket serverSocket() {
-		try {
-			return new ServerSocket(port);
-		} catch (IOException e) {
-			throw new ServerException("Failed to open server socket on port " + port, e);
-		}
-	}
+	public synchronized void stop() {
+		running = false;
 
-	private Socket acceptConnection(ServerSocket serverSocket) {
+		LOG.info("Shutting down web server executor.");
+
 		try {
-			return serverSocket.accept();
-		} catch (IOException e) {
-			LOG.info("Error accepting a client connection", e);
-			return null;
+			threadPool.shutdown();
+			threadPool.awaitTermination(timeout, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			LOG.error("Worker thread pool was interrupted while shutting down. Some client connections may have been terminated prematurely.", e);
+			Thread.currentThread().interrupt();
 		}
+
+		LOG.info("Web server executor has shutdown.");
 	}
 
 }
--- src/test/java/au/id/deejay/webserver/server/WebServerExecutorTest.java
+++ src/test/java/au/id/deejay/webserver/server/WebServerExecutorTest.java
@@ -1,17 +1,44 @@
 package au.id.deejay.webserver.server;
 
+import au.id.deejay.webserver.response.ResponseFactory;
+import org.junit.After;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author David Jessup
  */
 public class WebServerExecutorTest {
 
+	private WebServerExecutor executor;
+
+	@After
+	public void tearDown() throws Exception {
+		if (executor != null && executor.running()) {
+			executor.stop();
+		}
+	}
+
+	@Test
+	public void testConstructor() throws Exception {
+		executor = new WebServerExecutor(0, 10, 10, mock(ResponseFactory.class));
+		assertThat(executor.running(), is(false));
+	}
+
 	@Test
 	public void run() throws Exception {
+		executor = new WebServerExecutor(0, 10, 10, mock(ResponseFactory.class));
 
+		Thread executorThread = new Thread(executor);
+		executorThread.start();
+
+		await().until(executorThread::isAlive);
+
+		assertThat(executor.running(), is(true));
 	}
 
 	@Test

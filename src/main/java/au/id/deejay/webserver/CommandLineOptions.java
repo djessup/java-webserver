ad57bbf134db9ee3a1f3d47dedccfd13bed568bf
David Jessup <david.jessup@sydney.edu.au>
2016-10-10 16:57:51 +1100

    Increased default worker threads to 20

--- src/main/java/au/id/deejay/webserver/CommandLineOptions.java
+++ src/main/java/au/id/deejay/webserver/CommandLineOptions.java
@@ -28,7 +28,7 @@ public class CommandLineOptions {
 	/**
 	 * Default max number of worker threads.
 	 */
-	private static final int DEFAULT_MAX_THREADS = 8;
+	private static final int DEFAULT_MAX_THREADS = 20;
 
 	/**
 	 * Default request timeout.

04b8964efb36088446cdb252dbd81868eb069854
David Jessup <david.jessup@sydney.edu.au>
2016-10-18 14:42:18 +1100

    Added javadoc for webserver.handler

--- src/main/java/au/id/deejay/webserver/handler/DocrootHandler.java
+++ src/main/java/au/id/deejay/webserver/handler/DocrootHandler.java
@@ -70,11 +70,36 @@ public class DocrootHandler implements RequestHandler {
 		}
 	}
 
+	/**
+	 * Checks if the handler can service a request.
+	 *
+	 * @param request the request to be handled
+	 * @return Returns true if the request is a GET request.
+	 */
 	@Override
 	public boolean canHandle(Request request) {
 		return request.method() == HttpMethod.GET;
 	}
 
+	/**
+	 * Handles a GET request. The handler will first check if the request URI maps to a file or folder within the
+	 * docroot, and then response as follows:
+	 * <p>
+	 * If the requested file does not exist a "404 Not Found" response will be sent.
+	 * <p>
+	 * If the requested file does exist it will be streamed back to the client via a {@link FileResponse}.
+	 * <p>
+	 * If the requested file is actually a directory the handler will check the request has a trailing slash, and if
+	 * not will send a {@link RedirectResponse} to redirect the client to the correct directory URL.
+	 * <p>
+	 * If index files are enabled the handler will look for a matching file inside the directory, and if one is found it
+	 * will be served via a {@link FileResponse}.
+	 * <p>
+	 * Finally, if directory listings are enabled the handler will then send a {@link DirectoryListingResponse}.
+	 *
+	 * @param request the request to provide a response for
+	 * @return Returns a response message for the request.
+	 */
 	@Override
 	public Response handle(Request request) {
 		File requestedFile = child(request.uri().getPath());
@@ -111,6 +136,42 @@ public class DocrootHandler implements RequestHandler {
 		}
 	}
 
+	/**
+	 * Gets a child file inside the document root. Note this will return a valid {@link File}, even if the file does not
+	 * exist (it can be tested with {@link File#exists()}).
+	 *
+	 * @param name the name/path of the child file.
+	 * @return Returns a {@link File} object for the requested child file.
+	 */
+	private File child(String name) {
+		return new File(docroot, name);
+	}
+
+	/**
+	 * Checks if URI ends with a slash ("/") or not.
+	 *
+	 * @param uri the URI to check
+	 * @return Returns true if the URI does NOT end with a slash, or false if it does.
+	 */
+	private boolean uriDoesNotEndWithSlash(URI uri) {
+		return !uri.toString().endsWith("/");
+	}
+
+	/**
+	 * Checks if the index file options is enabled, and at least one index filename has been provided.
+	 *
+	 * @return Returns true if index files are enabled.
+	 */
+	private boolean indexFilesAreEnabled() {
+		return indexFiles != null && !indexFiles.isEmpty();
+	}
+
+	/**
+	 * Looks for a matching index file in a directory.
+	 *
+	 * @param directory the directory to search in.
+	 * @return Returns the first index file found, or null if no index file is found.
+	 */
 	private File findIndexFile(File directory) {
 
 		for (String indexFileName : indexFiles) {
@@ -121,17 +182,5 @@ public class DocrootHandler implements RequestHandler {
 		}
 
 		return null;
-	}
-
-	private boolean indexFilesAreEnabled() {
-		return indexFiles != null && !indexFiles.isEmpty();
-	}
-
-	private boolean uriDoesNotEndWithSlash(URI uri) {
-		return !uri.toString().endsWith("/");
-	}
-
-	private File child(String name) {
-		return new File(docroot, name);
 	}
 }
--- src/main/java/au/id/deejay/webserver/handler/ServerInfoHandler.java
+++ src/main/java/au/id/deejay/webserver/handler/ServerInfoHandler.java
@@ -1,22 +1,22 @@
 package au.id.deejay.webserver.handler;
 
-import au.id.deejay.webserver.api.RequestHandler;
+import au.id.deejay.webserver.api.*;
 import au.id.deejay.webserver.response.HttpResponse;
-import au.id.deejay.webserver.api.HttpStatus;
-import au.id.deejay.webserver.api.HttpMethod;
-import au.id.deejay.webserver.api.Request;
-import au.id.deejay.webserver.api.Response;
 
 import java.text.MessageFormat;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A request handler which provides a brief summary of the server's runtime configuration when a GET request is made
- * to /serverInfo
+ * to "/serverInfo".
  *
  * @author David Jessup
  */
 public class ServerInfoHandler implements RequestHandler {
 
+	/**
+	 * The path the server info will be served from
+	 */
 	private static final String SERVER_INFO_PATH = "/serverInfo";
 
 	private int port;
@@ -25,6 +25,15 @@ public class ServerInfoHandler implements RequestHandler {
 	private String docroot;
 	private long startTime;
 
+	/**
+	 * Creates a new {@link ServerInfoHandler}.
+	 *
+	 * @param port       the port the server is being run on
+	 * @param timeout    the client timeout, in seconds
+	 * @param maxThreads the maximum number of worker threads the server is configured to use
+	 * @param docroot    the path to the server's document root
+	 * @param startTime  the time the server was started (as reported by {@link System#currentTimeMillis()}
+	 */
 	public ServerInfoHandler(int port, int timeout, int maxThreads, String docroot, long startTime) {
 		this.port = port;
 		this.timeout = timeout;
@@ -33,26 +42,60 @@ public class ServerInfoHandler implements RequestHandler {
 		this.startTime = startTime;
 	}
 
+	/**
+	 * Checks that the request is a GET request to /serverInfo.
+	 *
+	 * @param request the request to be handled.
+	 * @return Returns true if the request is a GET request to the serverInfo path
+	 */
 	@Override
 	public boolean canHandle(Request request) {
 		return request.method() == HttpMethod.GET && SERVER_INFO_PATH.equals(request.uri().getPath());
 	}
 
+	/**
+	 * Sends a simple summary of the server configuration and uptime.
+	 *
+	 * @param request the request to provide a response for
+	 * @return Returns a 200 OK response containing a summary of the server.
+	 */
 	@Override
 	public Response handle(Request request) {
 
 		long uptime = (System.currentTimeMillis() - startTime) / 1000;
 
+		// Breakdown uptime into days/hours/mins/secs
+		long upDays = TimeUnit.SECONDS.toDays(uptime);
+		long upHours = TimeUnit.SECONDS.toHours(uptime) - (upDays * 24);
+		long upMins = TimeUnit.SECONDS.toMinutes(uptime) - (TimeUnit.SECONDS.toHours(uptime) * 60);
+		long upSecs = TimeUnit.SECONDS.toSeconds(uptime) - (TimeUnit.SECONDS.toMinutes(uptime) * 60);
+
+		String template = "<h1>Server info</h1>" +
+				"<table border='1' width='50%'>" +
+				"<tr>" +
+				"<th width='30%'>Uptime</th>" +
+				"<td width='70%'>{4}d {5}h {6}m {7}s</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<th>Port</th>" +
+				"<td>{0}</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<th>Timeout</th>" +
+				"<td>{1}s</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<th>Max worker threads</th>" +
+				"<td>{2}</td>" +
+				"</tr>" +
+				"<tr>" +
+				"<th>Document root</th>" +
+				"<td>{3}</td>" +
+				"</tr>" +
+				"</table>";
+
 		return new HttpResponse(HttpStatus.OK_200,
-								MessageFormat.format(
-										"Serving files from {0} on port {1} with {2} threads and a timeout of {3} seconds.\nUptime: {4}s",
-										docroot,
-										String.valueOf(port),
-										maxThreads,
-										timeout,
-										uptime
-								),
-								request.version()
-		);
+								MessageFormat.format(template, String.valueOf(port), timeout, maxThreads, docroot, upDays, upHours, upMins, upSecs),
+								request.version());
 	}
 }

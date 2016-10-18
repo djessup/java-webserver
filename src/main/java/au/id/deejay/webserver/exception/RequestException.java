afff63d4bb55dbb2710d7ead0e6f2a9f0f0e813f
David Jessup <david.jessup@sydney.edu.au>
2016-10-18 13:34:10 +1100

    Documented webserver.exception and removed unused ServerException

--- src/main/java/au/id/deejay/webserver/exception/RequestException.java
+++ src/main/java/au/id/deejay/webserver/exception/RequestException.java
@@ -1,26 +1,40 @@
 package au.id.deejay.webserver.exception;
 
 /**
+ * A {@link RequestException} is a runtime exception that indicates an abnormal state occurred during the generation or
+ * delivery of a {@link au.id.deejay.webserver.api.Request} message.
+ *
  * @author David Jessup
  */
 public class RequestException extends RuntimeException {
 
+	/**
+	 * @see RuntimeException#RuntimeException()
+	 */
 	public RequestException() {
 		super();
 	}
-
+	/**
+	 * @see RuntimeException#RuntimeException(String)
+	 */
 	public RequestException(String message) {
 		super(message);
 	}
-
+	/**
+	 * @see RuntimeException#RuntimeException(String, Throwable)
+	 */
 	public RequestException(String message, Throwable cause) {
 		super(message, cause);
 	}
-
+	/**
+	 * @see RuntimeException#RuntimeException(Throwable)
+	 */
 	public RequestException(Throwable cause) {
 		super(cause);
 	}
-
+	/**
+	 * @see RuntimeException#RuntimeException(String, Throwable, boolean, boolean)
+	 */
 	protected RequestException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
--- src/main/java/au/id/deejay/webserver/exception/ResponseException.java
+++ src/main/java/au/id/deejay/webserver/exception/ResponseException.java
@@ -1,26 +1,44 @@
 package au.id.deejay.webserver.exception;
 
 /**
+ * A {@link ResponseException} is a runtime exception that indicates an abnormal state occurred during the generation or
+ * delivery of a {@link au.id.deejay.webserver.api.Response} message.
+ *
  * @author David Jessup
  */
 public class ResponseException extends RuntimeException {
 
+	/**
+	 * @see RuntimeException#RuntimeException()
+	 */
 	public ResponseException() {
 		super();
 	}
 
+	/**
+	 * @see RuntimeException#RuntimeException(String)
+	 */
 	public ResponseException(String message) {
 		super(message);
 	}
 
+	/**
+	 * @see RuntimeException#RuntimeException(String, Throwable)
+	 */
 	public ResponseException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * @see RuntimeException#RuntimeException(Throwable)
+	 */
 	public ResponseException(Throwable cause) {
 		super(cause);
 	}
 
+	/**
+	 * @see RuntimeException#RuntimeException(String, Throwable, boolean, boolean)
+	 */
 	public ResponseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}

653fa7a499b73b56c4186898872833bb5a525760
David Jessup <david.jessup@sydney.edu.au>
2016-10-19 16:44:26 +1100

    Increase test coverage on WebWorker

--- src/test/java/au/id/deejay/webserver/server/WebWorkerTest.java
+++ src/test/java/au/id/deejay/webserver/server/WebWorkerTest.java
@@ -3,21 +3,30 @@ package au.id.deejay.webserver.server;
 import au.id.deejay.webserver.StringCollectorOutputStream;
 import au.id.deejay.webserver.api.HttpStatus;
 import au.id.deejay.webserver.api.HttpVersion;
+import au.id.deejay.webserver.api.Request;
 import au.id.deejay.webserver.api.Response;
+import au.id.deejay.webserver.io.ResponseWriter;
+import au.id.deejay.webserver.response.ErrorResponse;
 import au.id.deejay.webserver.response.HttpResponse;
 import au.id.deejay.webserver.response.ResponseFactory;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
 
+import static au.id.deejay.webserver.MessageConstants.CRLF;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author David Jessup
@@ -41,24 +50,84 @@ public class WebWorkerTest {
 	}
 
 	@Test
-	public void testValidRequestHandling() throws Exception {
-		withSocket();
+	public void testValidRequestIsHandledByResponseFactory() throws Exception {
 		withResponseFactory();
 		withValidRequest();
+		withSocket();
 		withWorker();
 
 		worker.run();
 
-		assertThat(response.status(), is(HttpStatus.OK_200));
+		assertResponseWasSentToClient();
+	}
+
+	@Test
+	public void testWorkerAbortsIfClientSocketIsBroken() throws Exception {
+		withBrokenSocket();
+		withResponseFactory();
+		withWorker();
+
+		worker.run();
+
+		verifyZeroInteractions(responseFactory);
+	}
+
+	@Test
+	public void testBadRequestReturns400Error() throws Exception {
+		withResponseFactory();
+		withBadRequest();
+		withSocket();
+		withWorker();
+
+		worker.run();
+
+		assertResponseWasSentToClient(Collections.singletonList(ErrorResponse.BAD_REQUEST_400));
+	}
+
+	@Test
+	public void testWorkerAbortsIfClientSocketTimesOut() throws Exception {
+		withResponseFactory();
+		withTimeoutSocket();
+		withWorker();
+
+		worker.run();
+
+		verifyZeroInteractions(responseFactory);
+	}
+
+	@Test
+	public void testKeepAliveBehaviour() throws Exception {
+
+
+	}
+
+	private void assertResponseWasSentToClient() {
+		assertResponseWasSentToClient(Collections.singletonList(this.response));
+	}
+
+	private void assertResponseWasSentToClient(Collection<Response> responses) {
+		OutputStream outputStream = new StringCollectorOutputStream();
+		ResponseWriter responseWriter = new ResponseWriter(outputStream);
+		for (Response response : responses) {
+			responseWriter.writeResponse(response);
+		}
+
+		assertThat(responseStream.toString(), is(equalTo(outputStream.toString())));
 	}
 
 	private void withValidRequest() throws Exception {
-		requestString = "GET /index.html HTTP/1.1\n" +
-			"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
-			"\n";
+		requestString = "GET /index.html HTTP/1.1" + CRLF
+			+ "Connection: close" + CRLF
+			+ CRLF;
 
 		response = new HttpResponse(HttpStatus.OK_200, "Test response", HttpVersion.HTTP_1_1);
 		when(responseFactory.response(any())).thenReturn(response);
+	}
+
+	private void withBadRequest() throws Exception {
+		requestString = "hurr durr" + CRLF
+			+ "derp derp derp" + CRLF
+			+ CRLF;
 	}
 
 	private void withSocket() throws Exception {
@@ -71,9 +140,26 @@ public class WebWorkerTest {
 
 	private void withResponseFactory() {
 		responseFactory = mock(ResponseFactory.class);
+		when(responseFactory.response(any(Request.class))).thenReturn(response);
 	}
 
 	private void withWorker() {
 		worker = new WebWorker(socket, responseFactory);
 	}
+
+	private void withBrokenSocket() throws Exception {
+		socket = mock(Socket.class);
+		when(socket.getInputStream()).thenThrow(IOException.class);
+		when(socket.getOutputStream()).thenThrow(IOException.class);
+	}
+
+	private void withTimeoutSocket() throws Exception {
+		socket = mock(Socket.class);
+		InputStream inputStream = mock(InputStream.class);
+		when(inputStream.read(any(), anyInt(), anyByte())).thenThrow(SocketTimeoutException.class);
+		when(socket.getInputStream()).thenReturn(inputStream);
+		responseStream = new StringCollectorOutputStream();
+		when(socket.getOutputStream()).thenReturn(responseStream);
+	}
+
 }
